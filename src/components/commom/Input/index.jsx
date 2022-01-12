cfc8fdc27ed67a12603f98884b02fd45a4a39633
Felipe-Sarmento <felipeleaosarmento@gmail.com>
2022-01-12 06:35:16 -0300

    feat: new feature to input component, applyes type

--- src/components/commom/Input/index.jsx
+++ src/components/commom/Input/index.jsx
@@ -1,11 +1,12 @@
 import { Container, BaseInput } from './styles';
 
-const Input = ({ variant, label, name, ...rest }) => (
+const Input = ({ variant, label, name, type, ...rest }) => (
     <Container variant = {variant}>
         {label && <label htmlFor={name}>{label}</label>}
         <BaseInput 
             id={name}
             name={name}
+            type={type}
             {...rest}
         />
     </Container>
