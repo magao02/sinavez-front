9d86c8af681a92581a1ecc465fd867392af80db8
Felipe-Sarmento <felipeleaosarmento@gmail.com>
2022-01-17 12:27:54 -0300

    feat: created state that controlls which part of signup form is rendered

--- src/pages/cadastro/index.jsx
+++ src/pages/cadastro/index.jsx
@@ -1,36 +1,26 @@
-import { Container, MainContent, InputForm, InputContainer } from './styles';
+import { useState } from 'react';
+
+import { Container, MainContent } from './styles';
 
 import Navigation from '../../components/commom/Nav';
-import Input from '../../components/commom/Input';
-import Select from '../../components/commom/Select';
-import MultiInput from '../../components/commom/MultiInput';
-import Button from '../../components/commom/Button';
+import SignUpFormFirst from '../../components/SignUpForm/SignUpFormFirst/SingUpFormFirst';
+import SignUpFormSecond from '../../components/SignUpForm/SignUpFormSecond/SingUpFormSecond';
+import SignUpFormThird from '../../components/SignUpForm/SignUpFormThird/SingUpFormThird';
 
-const SignUpPage = () => (
+const SignUpPage = () => {
+    
+    const [step, setStep] = useState(1);
+
+    return(
     <Container>
         <Navigation variant="singup" />
         <MainContent>
-            <InputForm>
-                <InputContainer>
-                    <Input variant="signup" label="Nome" name="nome" placeholder="Digite seu nome completo" />
-                    <Input variant="signup" label="Email" name="email" placeholder="email@domínio.com" />
-                    <Input variant="signup" label="Telefone" name="telefone" placeholder="(**) - ********" />
-                    <Input variant="signup" label="Data de Nascimento" name="data_de_nascimento" placeholder="DD/MM/AAAA"/>
-                </InputContainer>
-                <InputContainer>
-                    <Input variant="signup" label="CPF" name="cpf" placeholder="***.***.***-**" />
-                    <Input variant="signup" label="RG" name="rg" placeholder="*******.*" />
-                    <Input variant="signup" label="Data De Emissão" name="data_de_emissão" placeholder="DD/MM/AAAA" />
-                    <Input variant="signup" label="Data de Filiação" name="filiação" placeholder="Digite o(s) nome(s) completo(s)"/>
-                </InputContainer>
-                <InputContainer>
-                    <Select name="Profissão" profissoes={["lenhador", "pescador"]}></Select>
-                    <MultiInput label="Endereço" names= {["Rua", "Bairro", "Complemento", "Número", "Brasileiro?" ]}></MultiInput>
-                    <Button variant="signup">Próxima Página</Button>
-                </InputContainer>
-            </InputForm>
+            {step === 1 && <SignUpFormFirst/>}
+            {step === 2 && <SignUpFormSecond/>}
+            {step === 3 && <SignUpFormThird/>}
         </MainContent>
     </Container>
-);
+ )
+};
 
 export default SignUpPage;
--- src/pages/cadastro/styles.js
+++ src/pages/cadastro/styles.js
@@ -21,20 +21,3 @@ export const MainContent = styled.main`
     background-color: ${theme.colors.white};
 `;
 
-export const InputForm = styled.div`
-    width: 100%;
-
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-
-    gap: 5.6rem;
-
-    padding: 12.7rem 14rem 12.7rem 14rem;
-
-`;
-
-export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 2.4rem;
-`;
