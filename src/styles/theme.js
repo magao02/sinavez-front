23a6dbaf42690b67275e1b1702b8112e3b9544f1
SamuelLucasVM <samuellucasvmcontato@gmail.com>
2023-07-06 11:16:13 -0300

    style: adding new colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,23 +2,30 @@ import { ThemeConsumer } from "styled-components";
 
 const theme = {
     colors: {
-        white: '#FAFBFF',
+        white: {
+            'default': '#FAFBFF',
+            'light': '#FDFDFD'
+        },
         blue: {
             'light': '#EDF6FF',
             'default': '#0096B7',
             'heavy': '#0760BA',
             'shadow': '#C5DBF2',
-            'border': '#032E58'
+            'border': '#032E58',
+            'background': '#5D9BDA',
+            'button': '#2196F3',
+            'lighter': '#5D9BDA'
         },
         dark: {
             'light': '#404040',
             'default': '#252525',
-            'heavy': '#0E0C0C'  
+            'heavy': '#0E0C0C'
         },
         gray: {
             'default': '#747880',
             'heavy': '#A3A3A3',
-            'menu': '#3D3F45'
+            'menu': '#3D3F45',
+            'border': '#5D9BDA'
         },
         alert: '#0C014D',
         fail: '#F38B8B',
