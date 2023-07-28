00f5c11ce29c52be0fad20f8fb8a403bccb57a6d
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-07-28 19:43:44 -0300

    feat: add some styles

--- src/pages/apartamento/index.jsx
+++ src/pages/apartamento/index.jsx
@@ -1,20 +1,60 @@
 import {
-    NavSpacing,
-    Content,
+  NavSpacing,
+  Content,
+  Title1,
+  Subtitle2,
+  Details,
+  Column,
+  BlueOutlineCard,
+  Body1,
+  Body3,
+  Title2,
 } from "../../styles/apartamentoStyles";
 
 import Navigation from "../../components/commom/Nav";
+import Image from "next/image";
+
+import MapaImage from "../../assets/apartamento/mapa.png";
 
 const Page = () => {
-    return (
-      <div>
-        <Navigation selectedPage="apartamentos" variant="admin" />
-        <NavSpacing />
-        <Content>
-            hello
-        </Content>
-      </div>
-    );
+  return (
+    <div>
+      <Navigation selectedPage="apartamentos" variant="admin" />
+      <NavSpacing />
+      <Content>
+        <div>
+          <Title1>Apartamento 03</Title1>
+          <Body1>Apartamento Padrão (sem adaptação para PCD), 2 andar</Body1>
+        </div>
+        <Details>
+          <Column>
+            <BlueOutlineCard>
+              <div>
+                <Subtitle2>Itens do Apartamento</Subtitle2>
+              </div>
+            </BlueOutlineCard>
+
+            <BlueOutlineCard>
+              <div>
+                <Subtitle2>Áreas comuns</Subtitle2>
+                <Body3>Verifique a disponibilidade de uso das áreas comuns durante sua estadia. Caso queria usufruir de alguma em especifico faça a reserva aqui.</Body3>
+              </div>
+            </BlueOutlineCard>
+
+            <BlueOutlineCard>
+              <div>
+                <Subtitle2>Locais Próximos</Subtitle2>
+              </div>
+            </BlueOutlineCard>
+          </Column>
+
+          <Column></Column>
+        </Details>
+        <Title2>Veja a localização no mapa</Title2>
+        <Image src={MapaImage} />
+      </Content>
+    </div>
+  );
 };
-  
+
 export default Page;
--- src/styles/apartamentoStyles.js
+++ src/styles/apartamentoStyles.js
@@ -8,9 +8,91 @@ export const NavSpacing = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 16px;
   padding: 0 30px;
   @media(min-width: 1400px) {
     padding: 0 10vw;
   }
+  margin-top: 100px;
+`;
+
+export const Title1 = styled.h1`
+  color: ${theme.colors.gray.menu};
+
+  font-family: Jost;
+  font-size: 32px;
+  font-weight: 700;
+`;
+
+export const Title2 = styled.h2`
+  color: ${theme.colors.gray.menu};
+
+  font-family: Jost;
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+export const Subtitle1 = styled.h3`
+  color: ${theme.colors.gray.menu};
+
+  font-family: Jost;
+  font-size: 22px;
+  font-weight: 600;
+`;
+
+export const Subtitle2 = styled.h4`
+  color: ${theme.colors.gray.menu};
+
+  font-family: Jost;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const Body1 = styled.span`
+  color: ${theme.colors.gray.default};
+
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: 400;
+`;
+
+export const Body2 = styled.span`
+  color: ${theme.colors.gray.default};
+
+  font-family: Roboto;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const Body3 = styled.span`
+  color: ${theme.colors.gray.default};
+
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 400;
+`;
+
+export const Images = styled.div`
+`;
+
+export const Details = styled.div`
+  display: flex;
+  gap: 24px;
+  margin-top: 32px;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const BlueOutlineCard = styled.div`
+  display: flex;
+  padding: 24px;
+  flex-direction: column;
+  gap: 10px;
+
+  border-radius: 8px;
+  border: 1px solid ${theme.colors.blue.lighter};
+  background: ${theme.colors.white.default};
 `;
