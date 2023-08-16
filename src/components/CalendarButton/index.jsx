f88b00b97d1ac92e4374f468ad307012a5d34526
Igor Falcao <igoraaf1@gmail.com>
2023-08-15 21:47:24 -0300

    style/ alteracao no estilo do calendario

--- src/components/CalendarButton/index.jsx
+++ src/components/CalendarButton/index.jsx
@@ -3,69 +3,79 @@ import "flatpickr/dist/themes/light.css";
 import { CalendarWrapper, Container, ReservarButton } from "./styles";
 import { useEffect, useState } from "react";
 
-const CalendarButton = ({datas, setDatas}) => {
-    const [errorMsg, setErrorMsg] = useState(false);
+const CalendarButton = ({ datas, setDatas }) => {
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [showCalendar, setShowCalendar] = useState(false);
 
-    const formatDate = (date) => {
-
-        if(date != undefined){
-            var formattedDate = date.toLocaleDateString('pt-BR', {
-                day: 'numeric',
-                month: 'numeric',
-                year: 'numeric',
-            })
-            setErrorMsg(false)
-            return formattedDate;
-        }else{
-            setErrorMsg(true)
-        }
+  const formatDate = (date) => {
+    if (date != undefined) {
+      var formattedDate = date.toLocaleDateString("pt-BR", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+      });
+      setErrorMsg(false);
+      return formattedDate;
+    } else {
+      setErrorMsg(true);
     }
+  };
 
-    const verifyData = () => {
-        if(datas.length > 0){
-            return datas;
-        }else{
-            return "today"
-        }
+  const verifyData = () => {
+    if (datas.length > 0) {
+      return datas;
+    } else {
+      return "today";
     }
+  };
 
-    const defaultDate = () => {
-        return verifyData()
-    }
-    
-    return (
+  const defaultDate = () => {
+    return verifyData();
+  };
+
+  return (
+    <>
+      {showCalendar ? (
         <Container>
-            <CalendarWrapper>
-                {errorMsg ?
-                 <p style={{color: "red"}}>Preencha a Data Corretamente</p>
-                 :
-                 <p>Disponivel a partir do dia:</p>
-                 }
-                <Flatpickr options={{
-                    mode: "range",
-                    dateFormat: "d/m/Y",
-                    minDate:"today",
-                    locale: {
-                        rangeSeparator: " ate " 
-                    },
-                    defaultDate: defaultDate(),
-                    onClose: (e) => {
-                        try{
-                            var copy = []
-                            copy[0] = formatDate(e[0])
-                            copy[1] = formatDate(e[1])  
-                        }finally{
-                            setDatas(copy)
-                        }
-                    }
-                }
-                }/>
-            </CalendarWrapper>
-            <ReservarButton>
-                RESERVAR
-            </ReservarButton>
+          <CalendarWrapper>
+            {errorMsg ? (
+              <p style={{ color: "red" }}>Preencha a Data Corretamente</p>
+            ) : (
+              <p>Disponivel a partir do dia:</p>
+            )}
+            <Flatpickr
+              options={{
+                mode: "range",
+                dateFormat: "d/m/Y",
+                minDate: "today",
+                locale: {
+                  rangeSeparator: " ate ",
+                },
+                defaultDate: defaultDate(),
+                onClose: (e) => {
+                  try {
+                    var copy = [];
+                    copy[0] = formatDate(e[0]);
+                    copy[1] = formatDate(e[1]);
+                  } finally {
+                    setDatas(copy);
+                  }
+                },
+              }}
+            />
+          </CalendarWrapper>
+          <ReservarButton onClick={() => setShowCalendar(!showCalendar)}>RESERVAR</ReservarButton>
         </Container>
-    )
-}
+      ) : (
+        <ReservarButton
+          style={{ fontSize: "18px" }}
+          onClick={() => setShowCalendar(!showCalendar)}
+        >
+          RESERVAR
+        </ReservarButton>
+      )}
+    </>
+  );
+};
 
 export default CalendarButton;
