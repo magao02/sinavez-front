3c15c63e40ecd08924a3a96eabb1db8a193141d4
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-09-06 20:09:49 -0300

    feat: change index page to /choice

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,3 @@
-import { useRouter } from "next/router";
+import Page from "./choice";
 
-import { useAuth } from "../contexts/AuthContext";
-
-import { useEffect } from 'react';
-
-const root = () => {
-
-  const authContext = useAuth();
-  const router = useRouter();
-
-  const checkAuth = () => {
-      if (authContext == null) {
-        router.push('/login')
-      }
-      else {
-        router.push('/usuario')
-      };
-  }
-
-  useEffect(() => {
-    checkAuth()
-  }, [])
-
-  return(
-    <></>
-  )
-}
-
-export default root;
+export default Page;
