a2625abcbba15704a069d31f6427fe3c8d9042e2
lucas <lucas.araripina@hotmail.com>
2025-01-16 02:34:43 -0300

    adding new filters

--- src/pages/lancamentosAdmin/index.jsx
+++ src/pages/lancamentosAdmin/index.jsx
@@ -1,5 +1,5 @@
 import { useAuth } from "../../contexts/AuthContext";
-import { format } from 'date-fns';
+import { format,startOfMonth, endOfMonth } from 'date-fns';
 import { FaRegTrashAlt } from "react-icons/fa";
 import {
 
@@ -34,16 +34,23 @@ const cellStyle = {
   border: 'none',  // Removendo a borda
   lineHeight: '1.8', // Aumentando o espaçamento entre as linhas
 };
+
 import { useEffect, useState } from "react";
 import { cancelReservation } from "../../services/apartments";
 import styles from './style.module.css';
 import Navigation from "../../components/commom/Nav";
 import { Modal } from "../../components/lancamentoModal";
 import { addLanCamento, getLancamentos, deleteLancamento } from "../../services/lancamentos";
+import { SearchInput } from "../../components/SearchInputs";
 const Reservas = () => {
   const [apts, setApts] = useState([]);
   const [lancamentos, setLancamentos] = useState([]);
+  const [lancamentosFiltro, setLancamentosFiltro] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+    const today = new Date();
+  const [dataInicio, setDataInicio] = useState(startOfMonth(today));
+  const [dataFim, setDataFim] = useState(endOfMonth(today));
+
     const authContext = useAuth();
   useEffect( () => {
      getLancamentoss();
@@ -52,58 +59,12 @@ const Reservas = () => {
     const req = await getLancamentos(authContext.token);
     if (req.status == 200) {
       setLancamentos(req.data);
+      filterLancamentosDate();
+      
     }
-    console.log(req)
   };
 
-  const layouReserva = (apt, dado) => {
-    const cancelarReserva = async () => {
-      console.log(apt.urlApt, dado._id)
-      const req = await cancelReservation(authContext.token, apt.urlApt, dado._id);
-      if (req.status == 200) {
-        getReservas();
-      }
-    }
-    const status = () => {
-      console.log(dado)
-      if (dado.cancelled) {
-        return "Reserva cancelada"
-      } else if (dado.status != 'Cancelado' && new Date(dado.dataChegada) > new Date()) {
-        return "Reservado"
-      } else {
-        return "Reserva concluída"
-      }
-    }
-     return (
-    <Card  >
-      <CardImage reservado={true}>
-       
-        <img src={apt.fotos[0].url} alt="Imagem do apartamento" />
-      </CardImage>
-
-      <CardInner>
-        <Details>
-          <Title2>{apt.nome}</Title2>
-          <Body1>Data de Chegada: {new Date(dado.dataChegada).toLocaleDateString()}</Body1>
-             <Body2>Data de Saída: {new Date(dado.dataSaida).toLocaleDateString()} </Body2>
-             <Body2>Diária: R$ {dado.diaria},00</Body2>
-              <Body2>Dias: {dado.dias}</Body2>
-             <Body2>Hóspedes (Adultos): {dado.hospedes.adultos}</Body2>
-             <Body2>status: {status()}</Body2>
-             {status() == "Reservado" && 
-               <ButtonContainer>
-                  <StyledButton onClick={()=> cancelarReserva()} >Cancelar Reserva</StyledButton>
-
-              </ButtonContainer>
-              }
-         
-        </Details>
-        
-       
-      </CardInner>
-    </Card>
-  );
-  }
+  
 
   const onOpen = () => {
     setIsOpen(true)
@@ -113,6 +74,32 @@ const Reservas = () => {
     setIsOpen(false)
   }
 
+  const filterLancamentosDate = () => {
+    console.log(lancamentos)
+    const lancamentosFiltrados = lancamentos.filter((lancamento) => {
+      console.log(lancamento.data)
+      const data = new Date(lancamento.data);
+      if (dataInicio == null || dataFim == null) {
+        console.log("data nula")
+      }
+      if (data >= new Date(dataInicio) && data <= new Date(dataFim)) {
+        console.log("data no intervalo")
+      } else {
+        console.log("fora do intervalo")
+        console.log(dataInicio)
+        console.log(dataFim)
+       }
+
+      return data >= new Date(dataInicio) && data <= new Date(dataFim);
+    });
+    console.log(lancamentosFiltrados)
+    setLancamentosFiltro(lancamentosFiltrados);
+  }
+  
+  useEffect(() => {
+      filterLancamentosDate();
+
+  }, [dataInicio, dataFim, lancamentos]);
   
   const handleSave = async (dados) => {
     console.log(dados)
@@ -134,13 +121,33 @@ const Reservas = () => {
     }
   }
 
+  const setandodataInicio = (e) => {
+
+    const data = e.target.value;
+    setDataInicio(data);
+    filterLancamentosDate();
+
+  }
+  
+  const setandoDataFim = (e) => {
+    const data = e.target.value;
+    setDataFim(data);
+    filterLancamentosDate();
+  }
+
+
   return (
     <div>
       <Navigation selectedPage="reservas" variant={authContext?.admin ? "admin" : "logged"} />
       <NavSpacing />
       <Content>
         <div className={styles.lancamentoHeader}>
-          <h1>Lançamentos</h1> 
+          <h2>Lançamentos</h2> 
+          <div style={{width: '350px', display: 'flex', justifyContent: 'space-between', gap: '10px' }}>
+            <SearchInput style={{ width: '150px !important' }} label="Data ínicio" type="date" innerLabel="Data" initialValue={startOfMonth(today)} onChange={setandodataInicio} />
+            <SearchInput style={{ maxWidth: '150px' }} label="Data fim" type="date" innerLabel="Data"  initialValue={endOfMonth(today)} onChange={setandoDataFim}/>
+          </div>
+
           <Button variant='editButton' onClick={onOpen}> Novo lançamento</Button>
             
         </div>
@@ -157,7 +164,7 @@ const Reservas = () => {
           </tr>
         </thead>
         <tbody>
-          {lancamentos.map((item, index) => (
+          {lancamentosFiltro.map((item, index) => (
             <tr key={index}>
               <td style={{...cellStyle}} >{item.descricao}</td>
               <td style={{...cellStyle}}>{format(new Date(item.data), 'dd/MM/yyyy')}</td>
