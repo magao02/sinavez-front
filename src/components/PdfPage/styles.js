b0571520852a6efde87f6437e8fa054ef84f0980
lucas <lucas.araripina@hotmail.com>
2025-04-03 13:56:52 -0300

    feat: adjusting position

--- src/components/PdfPage/styles.js
+++ src/components/PdfPage/styles.js
@@ -4,6 +4,7 @@ import theme from "../../styles/theme";
 export const Container = styled.div`
   width: 100vw;
   height: 100%;
+  margin-top: 75px;
 
   display: grid;
   grid-template-columns: 1fr;
